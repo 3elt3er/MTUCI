3129dbf5a35ee7906dd51651439afda5979c811a
3elt3er <art.adilgereev@gmail.com>
2024-01-17 12:01:26 +0300

    Изменена структура проекта

---
+++ src/js/theme.js
@@ -0,0 +1,43 @@
+let head = window.document.getElementsByTagName('head')[0]
+
+// localStorage.setItem('isDarkMode', true)
+function includeCSS(aFile, aRel){
+    let style = window.document.createElement('link')
+    style.href = aFile
+    style.rel = aRel || 'stylesheet'
+    style.id = 'theme'
+    console.log('includeCSS')
+    head.appendChild(style)
+}
+
+function deleteCSS(){
+    theme.remove();
+    console.log('deleteCSS')
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    if (localStorage.getItem('isDarkMode') == 'false') {
+        deleteCSS()
+        console.log('Включена светлая тема')
+    } else if (localStorage.getItem('isDarkMode') == 'true') {
+        console.log('Включена темная тема')
+    } else {
+        localStorage.setItem('isDarkMode', true)
+    }
+
+});
+
+const themeIcon = document.getElementById('themeIcon');
+
+if (themeIcon) {
+    themeIcon.addEventListener('click', ()=> {
+        if (localStorage.getItem('isDarkMode') === 'true') {
+            deleteCSS()
+            localStorage.setItem('isDarkMode', false)
+        } else {
+            includeCSS('src/styles/blackTheme.css')
+            localStorage.setItem('isDarkMode', true)
+        }
+    })
+}
