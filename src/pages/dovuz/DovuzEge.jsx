35401474a20b6db74e4882133049f9e5ccd80120
Fourart <fourart@MacBook-Air-Fourart.local>
2025-08-12 15:39:29 +0300

    update_dovuz11ege

--- src/pages/dovuz/DovuzEge.jsx
+++ src/pages/dovuz/DovuzEge.jsx
@@ -30,12 +30,23 @@ const DovuzEge = () => {
               <span>4. Физика</span>
             </div>
           </div>
-          <div className={classes.columns}>
+            <div className={classes.columns}>
             <div className={`${classes.item} ${classes.firstItem}`}>
               <div className={classes.header}>Стоимость</div>
-              <span>Полный курс по одному предмету 59 200 рублей за один предмет.</span>
-              <span>Оплата ежемесячно по  7 400 рублей за один предмет</span>
+              <span>Стоимость курса (32 занятия) - 59200Р</span>
+              <span>Стоимость 1 месяца (4 занятия) - 7400Р</span>
             </div>
+            <div className={`${classes.item} ${classes.firstItem}`}>
+            <div className={classes.header}>Стоимсоть если брать 3 предмета, то скидка 10%</div>
+            <span>Стоимость курса - 53280Р</span>
+            <span>Стоимость 1 месяца - 6660Р</span>
+              </div>
+              <div className={`${classes.item} ${classes.firstItem}`}>
+            <div className={classes.header}>Стоимсоть если брать 4 предмета, то скидка 15%</div>
+            <span>Стоимость курса - 50320Р</span>
+            <span>Стоимость 1 месяца - 6290Р</span>
+              </div>
+              </div>
             <div className={classes.columns}>
               <div className={`${classes.item} ${classes.firstItem}`}>
                 <div className={classes.header}>Формат обучение</div>
@@ -49,7 +60,6 @@ const DovuzEge = () => {
                 <span>2. Русский язык с 13:30 до 16:45</span>
               </div>
             </div>
-            <div className={classes.columns}>
               <div className={`${classes.item} ${classes.firstItem}`}>
                 <div className={classes.header}>Расписание</div>
                  <span>Продолжительность: 32 занятия по 4 ак.часа (всего 128 ак.часов)</span>
@@ -57,8 +67,6 @@ const DovuzEge = () => {
                  <span>1 занятие: с 10:00 до 13:15</span>
                  <span>2 занятие: с 13:30 до 16:45</span>
               </div>
-            </div>
-          </div>
         </div>
       </div>
       <div className='window noHeightLimitMobile' id='advantages'>
